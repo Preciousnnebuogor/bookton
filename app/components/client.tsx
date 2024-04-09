db3a7c4d3eef3cb03f809c19669cd282bcb02359
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-04-09 21:47:03 +0100

    deleted comps

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -1,19 +1,11 @@
 
-import React from "react"
-import Hero from "./hero"
-import About from "./about"
-import Section from "./section"
-import Detail from "./detail"
 
 
 export default function HomePage() {
 
   return (
     <div className={"bg-secondary"}>
-      <Hero/>
-      <About/>
-      <Section/>
-      <Detail/>
+      
     </div>
 
   )
