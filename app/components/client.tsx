8f9f8adc57bdd5c237ce41d271e45664ca2d74a6
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-04-10 21:04:48 +0100

    added hero section

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -1,13 +1,9 @@
-
-
-
+import Hero from "./hero"
 
 export default function HomePage() {
-
   return (
     <div className={"bg-secondary"}>
-      
+      <Hero />
     </div>
-
   )
 }
--- lib/appImages.ts
+++ lib/appImages.ts
@@ -1,5 +1,7 @@
 export const ImageApp = {
   logo: "/logo.png",
+  hero1: "/man.png",
+  hero2: "/woman.png",
 }
 
 
