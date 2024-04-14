7cdff8dc7c4fdf6d285ba3c5e43786fd4c4d8acf
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-04-14 21:45:21 +0100

    made hero section mobile responsive

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -7,13 +7,13 @@ import { ImageApp } from "@/lib/appImages"
 export default function Hero() {
   return (
     <div className={"h-[calc(100vh-60px)]"}>
-      <div className={`grid grid-cols-2 md:grid-cols-[60%_40%] px-8 gap-x-6`}>
+      <div className={`grid grid-cols-1 md:grid-cols-[60%_40%] px-8 gap-x-6`}>
         <div
           className={`rounded-3xl bg-zinc-400 p-2 border-zinc-400 border-2 relative `}
         >
           <div className={"flex  justify-between"}>
             <div className={""}>
-              <p className={" text-5xl text-white mt-[50px] w-5"}>
+              <p className={" md:text-5xl text-2xl text-white mt-[50px] w-5"}>
                 Ebook learning skill completed
               </p>
               <p className={"text-sm text-white mt-2"}>more offer</p>
@@ -28,12 +28,12 @@ export default function Hero() {
             </div>
             <img
               src={ImageApp.hero2}
-              className={" h-[400px] w-[400px] absolute right-4 bottom-0"}
+              className={" h-[200px] w-[200px] md:h-[400px] md:w-[400px] absolute right-4 bottom-0"}
             />
           </div>
         </div>
 
-        <div className={`h-full flex flex-col justify-between`}>
+        <div className={`h-full flex flex-col justify-between md:mt-0 mt-10`}>
           <div
             className={`rounded-3xl bg-zinc-200 border-zinc-200 border-2 p-4 mb-4 flex justify-between items-center gap-6`}
           >
@@ -43,7 +43,7 @@ export default function Hero() {
           <div
             className={`rounded-3xl  border-2 p-4  bg-zinc-400 border-zinc-400 `}
           >
-            <div className={"flex items-center justify-center"}>
+            <div className={"flex items-center justify-center md:pl-0 pl-8 "}>
             <p className={"text-sm h-[150px] "}>Here are some samples you can trust us with</p>
             <img src={ImageApp.hero1} className={"h-[200px] w-full"} />
             </div>
@@ -54,3 +54,4 @@ export default function Hero() {
     </div>
   )
 }
+ 
--- app/product/page.tsx
+++ app/product/page.tsx
@@ -2,12 +2,17 @@ import { ProductList } from "./data"
 
 export default function ProductPage() {
   return (
+    <>
     <div>
       <div>
         {ProductList.map((value,index) => (
-          <div>{value.name}</div>
+          <div>{value.name}
+          {value.price}
+          </div>
+          
         ))}
       </div>
     </div>
+    </>
   )
 }
