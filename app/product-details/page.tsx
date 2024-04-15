3751d2ff588de7b4f4734eca1eb1389b1630e48e
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-04-15 12:59:50 +0100

    added product-details page

--- app/product-details/page.tsx
+++ app/product-details/page.tsx
@@ -1,5 +1,20 @@
+import { ImageApp } from "@/lib/appImages";
+
 export default function ProductDetails(){
     return <div>
-        productdetail
+        <div className={"grid grid-cols-1 md:grid-cols-[50%_50%] gap-x-6 px-10"}>
+            <div className={`bg-zinc-200 mb:mb-0 mb-4`}>
+            <img src={ImageApp.hero1}/>
+            </div>
+            <div>
+
+                <div>
+                    <p>Template Image</p>
+                </div>
+                <p>price</p>
+                <div><button>Buy me</button></div>
+
+            </div>
+        </div>
     </div>
 }
