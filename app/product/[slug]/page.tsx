b822c016197b7db3fbada54071954dc73098e66b
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-04-18 21:30:42 +0100

    completed product page

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -5,7 +5,7 @@ import { ProductList } from "../data"
 export default function Page({ params }: { params: { slug: string } }) {
   const product = ProductList.filter(
     (value, index) => index.toString() === params.slug
-  ) [0]
+  )[0]
   return (
     <div>
       <div className={"grid grid-cols-1 md:grid-cols-[50%_50%] gap-x-6 px-10"}>
@@ -14,20 +14,17 @@ export default function Page({ params }: { params: { slug: string } }) {
         </div>
         <div>
           <div className={`w-[350px]`}>
-            <h1 className={`text-xl mb-2 font-bold md:mt-6`}>
-              {product.name}
-            </h1>
-            <p className={` text-xs mb-4`}>
-              {product.desc}
-            </p>
-            
+            <h1 className={`text-xl mb-2 font-bold md:mt-6`}>{product.name}</h1>
+            <p className={` text-xs mb-4`}>{product.desc}</p>
           </div>
           <p className={`text-2xl mb-10 font-bold`}>${product.price}</p>
           <div
             className={`flex items-center justify-center rounded-3xl 
                 border-2 bg-primary p-4 font-bold`}
           >
-             <a href="/ebook/1.pdf" download="1"><button>Buy Now</button></a>
+            <a href={product.bookpath} download={product.name}>
+              <button>Buy Now</button>
+            </a>
           </div>
         </div>
       </div>
